0014793b64dd895e280aca59632735cf5cd85fef
Wojciech Grochalski <alchemik789@gmail.com>
2021-02-10 19:22:40 +0100

    Login page update1

---
+++ ClientApp/src/Components/set-alert/set-alert.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-set-alert',
+  templateUrl: './set-alert.component.html',
+  styleUrls: ['./set-alert.component.css']
+})
+export class SetAlertComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
